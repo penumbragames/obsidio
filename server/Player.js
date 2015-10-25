cc9b4a57450bfc9499fb8abecec3edba81abc87f
Alvin Lin <alin18@stuy.edu>
2015-10-25 03:21:38 -0400

    build inside radius

--- server/Player.js
+++ server/Player.js
@@ -130,9 +130,9 @@ Player.prototype.updateOnInput = function(keyboardState, orientation, shot,
   }
 
 
-//      Util.getEuclideanDistance(this.x, this.y, build.x, build.y) <
-//      Constants.CONSTRUCT_BUILD_RADIUS
   if (build &&
+      Util.getEuclideanDistance(this.x, this.y, build.x, build.y) <
+      Constants.CONSTRUCT_BUILD_RADIUS &&
       this.praesidia >= Constants.CONSTRUCT_REQUIREMENT[build.type]) {
     addConstructCallback(Construct.create(build.x, build.y, 0, this.id,
                                           build.type));
