8a8e23bb67350e9642bda1dce7bb27315cf3a8bd
Alvin Lin <alvin.lin.dev@gmail.com>
2015-10-31 20:52:45 -0400

    documented base.js

--- static/js/base.js
+++ static/js/base.js
@@ -3,12 +3,25 @@
  * @author Alvin Lin (alvin.lin.dev@gmail.com)
  */
 
+/**
+ * Binds a function to a context, useful for assigning event handlers and
+ * function callbacks.
+ * @param {Object} context The context to assign the method to.
+ * @param {function()} method The method to bind the context to.
+ */
 function bind(context, method) {
   return function() {
     return method.apply(context, arguments);
   };
 }
 
+/**
+ * Creates and returns a div element.
+ * @param {?Element=} parent The element to append the newly created div to.
+ * @param {?string=} id The id to apply to the newly created div.
+ * @param {?string=} className The class name to apply to the newly created
+ *   div.
+ */
 function createDiv(parent, id, className) {
   var div = document.createElement('div');
   if (parent) {
@@ -23,6 +36,12 @@ function createDiv(parent, id, className) {
   return div;
 }
 
+/**
+ * Creates a returns a JavaScript Image object.
+ * @param {?string=} src The source URL of the image.
+ * @param {?string=} width The width of the image in pixels.
+ * @param {?string=} height The height of the image in pixels.
+ */
 function createImage(src, width, height) {
   var image = new Image(width, height);
   image.src = src;
